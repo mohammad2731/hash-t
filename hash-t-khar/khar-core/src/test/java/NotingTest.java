a368a26b0673a306985ff368c89e5180c7e749d9
Mohammad <Mohammad.Khodadadi@erstegroup.com>
2025-09-22 16:32:37 +0200

    Add a dummy test

---
+++ hash-t-khar/khar-core/src/test/java/NotingTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NotingTest {
+
+    @Test
+    void nothing(){
+        assertTrue(true);
+    }
+}
